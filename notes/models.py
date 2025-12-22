31a921e9fbcd3a4ab00844fb7b027744274572e9
haykel <haykel.j@gmail.com>
2025-12-22 09:25:22 +0100

    implement note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    """
+    Textual note that can be linked to multiple todos.
+    """
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
